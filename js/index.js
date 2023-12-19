fc221813bfb1c71fdfeaeb4829d6cda41bb5ca40
EloiVega <eslamonsy56@gmail.com>
2023-12-19 16:09:33 +0200

    added labels' list

--- js/index.js
+++ js/index.js
@@ -18,9 +18,97 @@ $(document).ready(function() {
             - don't over-traverse through the list when scrolling: highlightedTopic = max(list.length-1, highlightedTopic + 1)
     */
 
+
+    const Labels = [
+        {
+            label: "skin",
+            image_src: "./ui/images/skin.jpg",
+            alt: "An illustration of skin",
+            xPosition: 0,
+            yPosition: 0,
+        },
+        {
+            label: "bones",
+            image_src: "./ui/images/skeleton.jpg",
+            alt: "An illustration of bones",
+            xPosition: 10,
+            yPosition: 0,
+        },
+        {
+            label: "joints",
+            image_src: "./ui/images/joints.jpg",
+            alt: "An illustration of joints",
+            xPosition: 20,
+            yPosition: 0,
+        },
+        {
+            label: "perephiral nerves",
+            image_src: "./ui/images/nerves.jpg",
+            alt: "An illustration of nerves",
+            xPosition: 30,
+            yPosition: 0,
+        },
+        {
+            label: "teeth",
+            image_src: "./ui/images/teeth.jpg",
+            alt: "An illustration of teeth",
+            xPosition: 40,
+            yPosition: 0,
+        },
+        {
+            label: "heart",
+            image_src: "./ui/images/heart.jpg",
+            alt: "An illustration of heart",
+            xPosition: 50,
+            yPosition: 0,
+        },
+        {
+            label: "blood vessels",
+            image_src: "./ui/images/blood.jpg",
+            alt: "An illustration of blood vessels",
+            xPosition: 60,
+            yPosition: 0,
+        },
+        {
+            label: "bone marrow",
+            image_src: "./ui/images/bonemarrow.jpg",
+            alt: "An illustration of bone marrow",
+            xPosition: 70,
+            yPosition: 0,
+        },
+        {
+            label: "muscles",
+            image_src: "./ui/images/muscles.jpg",
+            alt: "An illustration of muscles",
+            xPosition: 80,
+            yPosition: 0,
+        },
+        {
+            label: "retina",
+            image_src: "./ui/images/eye.jpg",
+            alt: "An illustration of retina",
+            xPosition: 90,
+            yPosition: 0,
+        },
+        {
+            label: "thyroid",
+            image_src: "./ui/images/thyroid.jpg",
+            alt: "An illustration of thyroid",
+            xPosition: 0,
+            yPosition: 10,
+        },
+        {
+            label: "kidneys",
+            image_src: "./ui/images/kidney.jpg",
+            alt: "An illustration of a kidney",
+            xPosition: 20,
+            yPosition: 10,
+        },
+    ]
+
     var lastScrollTop = 0;
     var isScrolling = false;
-    $(document).scroll((event) => {
+    $(window).scroll((event) => {
 
         if(isScrolling) return;
         isScrolling = true;
