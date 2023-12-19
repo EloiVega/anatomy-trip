7018e79c8fe86e5d989638cfa0f63aa912c1b572
EloiVega <eslamonsy56@gmail.com>
2023-12-19 20:06:06 +0200

    final touches

--- js/index.js
+++ js/index.js
@@ -1,108 +1,88 @@
 $(document).ready(function() {
-    // var scrollorama = $.scrollorama({ blocks:'.scrollblock' });
-
-    //to do:
-    /*
-        ✔️ - Create a list where you can store each topic.
-            ex. [
-                {
-                    label: string,      --> name of the topic
-                    image_src: string,  --> Source of the image related to the label
-                    alt: string,        --> Alternative text for the image for people with unsupporting websites or using text narration tools
-                    xPosition: int,     --> sets its horizontal position on the skeleton
-                    yPosition: int,     --> sets its vertical position on the skeleton
-                }
-            ]
-        - To avoid Bugs:
-        ✔️ - don't forget to set the maximum scroll to list.length * sizeOfScrollBlock
-            - don't over-traverse through the list when scrolling: highlightedTopic = max(list.length-1, highlightedTopic + 1)
-    */
-
-
     const labels = [
         {
             label: "skin",
             image_src: "./ui/images/skin.jpg",
             alt: "An illustration of skin",
-            xPosition: 0,
-            yPosition: 0,
+            xPosition: 79,
+            yPosition: 42,
         },
         {
             label: "bones",
             image_src: "./ui/images/skeleton.jpg",
             alt: "An illustration of bones",
-            xPosition: 10,
-            yPosition: 0,
+            xPosition: 43,
+            yPosition: 85,
         },
         {
             label: "joints",
             image_src: "./ui/images/joints.jpg",
             alt: "An illustration of joints",
-            xPosition: 20,
-            yPosition: 0,
+            xPosition: 55,
+            yPosition: 72,
         },
         {
             label: "perephiral nerves",
             image_src: "./ui/images/nerves.jpg",
             alt: "An illustration of nerves",
-            xPosition: 30,
-            yPosition: 0,
+            xPosition: 42,
+            yPosition: 40,
         },
         {
             label: "teeth",
             image_src: "./ui/images/teeth.jpg",
             alt: "An illustration of teeth",
-            xPosition: 40,
-            yPosition: 0,
+            xPosition: 48.5,
+            yPosition: 17,
         },
         {
             label: "heart",
             image_src: "./ui/images/heart.jpg",
             alt: "An illustration of heart",
-            xPosition: 50,
-            yPosition: 0,
+            xPosition: 55,
+            yPosition: 30,
         },
         {
             label: "blood vessels",
             image_src: "./ui/images/blood.jpg",
             alt: "An illustration of blood vessels",
-            xPosition: 60,
-            yPosition: 0,
+            xPosition: 20,
+            yPosition: 40,
         },
         {
             label: "bone marrow",
             image_src: "./ui/images/bonemarrow.jpg",
             alt: "An illustration of bone marrow",
-            xPosition: 70,
-            yPosition: 0,
+            xPosition: 28,
+            yPosition: 32,
         },
         {
             label: "muscles",
             image_src: "./ui/images/muscles.jpg",
             alt: "An illustration of muscles",
-            xPosition: 80,
-            yPosition: 0,
+            xPosition: 42,
+            yPosition: 28.5,
         },
         {
             label: "retina",
             image_src: "./ui/images/eye.jpg",
             alt: "An illustration of retina",
-            xPosition: 90,
-            yPosition: 0,
+            xPosition: 46,
+            yPosition: 11.5,
         },
         {
             label: "thyroid",
             image_src: "./ui/images/thyroid.jpg",
             alt: "An illustration of thyroid",
-            xPosition: 0,
-            yPosition: 10,
+            xPosition: 48,
+            yPosition: 23,
         },
         {
             label: "kidneys",
             image_src: "./ui/images/kidney.jpg",
             alt: "An illustration of a kidney",
-            xPosition: 20,
-            yPosition: 10,
+            xPosition: 55,
+            yPosition: 40,
         },
     ]
 
@@ -117,6 +97,10 @@ $(document).ready(function() {
 
     // Marking and Unmarking Logic //
     const markTopic = (index) => {
+        if(timer){ //Prevent the user from performing too many updates at a time
+            return;
+        }
+
         let id = removeWhiteSpaces(labels[markedPos].label);
         //unmark the previous markedPos
         $(`#${id}`).removeClass('marked');
@@ -129,13 +113,11 @@ $(document).ready(function() {
         $(`#${id}_label`).addClass('marked_label');
 
         const designatedScrollPosition = index * scrollBlockHeight;
-
-        isScrolling= false;
-        if(timer) clearTimeout(timer);
+        console.log(index);
         
         isScrolling = true;
-        $('html, body').animate({scrollTop: designatedScrollPosition}, 700);
-        timer = setTimeout(()=>{isScrolling = false}, 800);
+        $('html, body').animate({scrollTop: designatedScrollPosition}, 500);
+        timer = setTimeout(()=>{isScrolling = false; timer = null}, 550);
     }
 
     // SCROLLING LOGISTICS //
@@ -144,21 +126,22 @@ $(document).ready(function() {
     var lastScrollTop = 0;
     var isScrolling = false;
     $(window).scroll((event) => {
-
         if(isScrolling) return;
         isScrolling = true;
-        setTimeout(() => {isScrolling = false}, 500);
 
         var startingScollPosition = $(this).scrollTop();
         var designatedScrollPosition = lastScrollTop;
 
         if(startingScollPosition > lastScrollTop){
             designatedScrollPosition = Math.min(maximumScrollPosition, lastScrollTop + 1048);
+
         } else {
                 designatedScrollPosition = lastScrollTop - 1048;
         }
 
-        $('html, body').animate({scrollTop: designatedScrollPosition}, 300);
+        let index = designatedScrollPosition / 1048;
+        if(!timer)
+            markTopic(index);
         lastScrollTop = designatedScrollPosition;
     })
 
